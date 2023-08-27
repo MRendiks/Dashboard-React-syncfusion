02ac3df4e70d4752115ef79fcb01c7bec32720e2
Rendaks <105725708+MRendiks@users.noreply.github.com>
2023-08-27 23:02:44 +0700

    add initial setup for project

---
+++ src/components/Charts/SparkLine.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LineChart = () => {
+  return (
+    <div>SparkLine</div>
+  )
+}
+
+export default LineChart
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LineChart = () => {
+    return (
+        <div>Footer</div>
+    )
+}
+
+export default LineChart
